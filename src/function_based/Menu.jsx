27d788d13c046ab36e5381501881183370e503f8
Rahul <rahulbadiger072gmail.com>
2023-03-03 22:28:18 +0530

    done

---
+++ src/function_based/Menu.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Component } from "react";
+const Menu=()=>{
+    return(
+        <div className="menu">
+        <ul >
+            <li >
+                <a href="">HOME</a>
+            </li>
+            <li>
+                <a href="">LOGIN</a>
+            </li>
+            <li>
+                <a href="">SIGNUP</a>
+            </li>
+            <li>
+                <a href="">ABOUT</a>
+            </li>
+        
+        </ul>
+        </div>
+    )
+}
+export default Menu;
--- src/function_based/Nav.jsx
+++ src/function_based/Nav.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import Logo from './Logo';
+import Menu from './Menu';
 const Nav=()=>{
     return(
-        <div>Navbar</div>
+        <>
+        <div className='nav'>
+         <Logo/>
+       <Menu/>
+       </div> 
+      
+       </>
     )
 }
 export default Nav;
