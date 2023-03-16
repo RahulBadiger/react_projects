eda3bfdad44f64de697644ac6b25a816c35583b3
Rahul <rahulbadiger072gmail.com>
2023-03-16 19:27:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
 import React, { Component } from 'react'
+import Hooks from './function_based/Hooks'
 
- class App extends Component {
-  constructor (){
-    super()
-    this.state={
-      count:0
-
-    }
-  }
-  handleIncrement=()=>{
-    this.setState({count:this.state.count+1})
-  }
-  handleDecrement=()=>{
-    this.setState({count:this.state.count-1})
-  }
-  handleReset=()=>{
-    this.setState({count:0})
-  }
+export default class App extends Component {
   render() {
     return (
-      <div style={{marginLeft:"500px",marginTop:"200px"}}>
-        <h2 style={{marginLeft:"220px",fontSize:"50px",textShadow:"2px 2px "}}>{this.state.count}</h2>
-        <div >
-        <button onClick={this.handleIncrement} style={{boxShadow:"4px 4px ",fontSize:"30px",backgroundColor:"green"}}>+ Increment</button>
-        <button onClick={this.handleDecrement} style={{boxShadow:"4px 4px",fontSize:"30px",backgroundColor:"yellow"}}>- Decrement</button>
-        <button onClick={this.handleReset} style={{boxShadow:"4px 4px",fontSize:"30px",backgroundColor:"red"}}>Reset</button>
-        </div>
-      </div>
+      <div><Hooks/></div>
     )
   }
 }
-export default App
