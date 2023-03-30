84c314def1cede9149b4866f183e32db66c13ea8
Rahul <rahulbadiger072gmail.com>
2023-03-31 00:00:08 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Home1 from './Routers1/Rcomponents/Home1';
-import Nav1 from './Routers1/Rcomponents/Nav1';
-import Signup1 from './Routers1/Rcomponents/Signup1';
-
+import CreatePost from './components/CreatePost'
 
 
 const App = () => {
   return (
-    <Router>
-        <Routes>
-            <Route path='/' element={<Home1/>}></Route>
-            <Route path='/Nav' element={<Nav1></Nav1>}></Route>
-            <Route path='/sign' element={<Signup1></Signup1>}></Route>
-
-        </Routes>
-    </Router>
-
+    <>
+    <CreatePost/>
+    </>
   )
 }
 
